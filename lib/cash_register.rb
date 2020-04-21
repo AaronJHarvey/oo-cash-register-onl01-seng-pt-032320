b97c2a844d0d2f0b0a6b01dab79676cdd4442db0
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-21 01:31:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,8 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :last_transaction_amount, :items 
   
 def initialize(discount = 0)
+  @total = 0 
+  
 end
 end
