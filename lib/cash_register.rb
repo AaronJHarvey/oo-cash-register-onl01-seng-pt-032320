afc16e37e2ec2b1838736d3a30677b3ba2840988
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-21 02:26:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,14 +3,14 @@ attr_accessor :total, :discount, :items, :last_transaction
 
 def initialize(discount = 0)
   @total = 0 
-  items = []
+  @items = []
   @discount = discount
 end
 
 def add_item(title, price, quantity = 1)
   @total += price * quantity
   quantity.times do
-    items << title 
+    @items << title 
 end
 end
 end
